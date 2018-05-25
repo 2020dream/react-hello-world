aa4715918ab1b9d0a6488ed49e8f35093484bc24
Zheng Cao <chori84@gmail.com>
2018-05-25 11:17:25 -0700

    Implement render() in Student

--- src/components/Student.js
+++ src/components/Student.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 
 class Student extends Component {
 
+  render() {
+    return (
+      <article>
+        <h3>Persy Gets an A</h3>
+        <p>Age: 37</p>
+        <p>Email: percy@sample.com</p>
+      </article>
+    );
+  }
+
 }
 
 export default Student;
