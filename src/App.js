351785274e7626b4ff967133645874b69bb2ab14
Zheng Cao <chori84@gmail.com>
2018-05-25 11:11:31 -0700

    Create Student Component

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+
+    const name = "Dee";
+
+    // Two ways to dynamically render variable value in HTML.
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React, {name} is totally awesome.</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+          Hello World!!! {`${name} is totally awesome.`}
+        </p>
+        <img src="http://via.placeholder.com/350x150" className="trek" alt="trek" />
+      </div>
+    );
+  }
+}
+
+export default App;
