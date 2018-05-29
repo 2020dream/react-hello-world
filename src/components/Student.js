b488aaad42246f0d81dfb6018139749efd747a2b
Zheng Cao <chori84@gmail.com>
2018-05-29 10:00:21 -0700

    Inline style (not recommendedgit add .)

--- src/components/Student.js
+++ src/components/Student.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 
 class Student extends Component {
 
+  // static propTypes = {
+  //   name: PropTypes.string.isRequired,
+  //   email: PropTypes.string.isRequired,
+  // }
+
   render() {
+    const studentStyle = {
+      color: 'purple',
+      fontSize: '14pt',
+    }
+
     const studentName = this.props.studentData.name;
     const studentEmail = this.props.studentData.email;
     const studentAge = this.props.studentData.age;
 
     return (
-      <article>
+      <article style={studentStyle}>
         <h3>{studentName}</h3>
         <p>Age: {studentAge}</p>
         <p>Email: {studentEmail}</p>
