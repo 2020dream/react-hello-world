b2c7b2b9dbe48305a24b5c299f16f082078f30c8
Zheng Cao <chori84@gmail.com>
2018-05-29 10:04:10 -0700

    Use CSS file for styling (recommendedgit add .)

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Student.css';
 
 class Student extends Component {
 
-  // static propTypes = {
-  //   name: PropTypes.string.isRequired,
-  //   email: PropTypes.string.isRequired,
-  // }
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+  }
 
   render() {
-    const studentStyle = {
-      color: 'purple',
-      fontSize: '14pt',
-    }
+    // const studentStyle = {
+    //   color: 'purple',
+    //   fontSize: '14pt',
+    // }
 
     const studentName = this.props.studentData.name;
     const studentEmail = this.props.studentData.email;
     const studentAge = this.props.studentData.age;
 
     return (
-      <article style={studentStyle}>
+      <article className='student'>
         <h3>{studentName}</h3>
         <p>Age: {studentAge}</p>
         <p>Email: {studentEmail}</p>
