42e332ddc46633b664af2c69ddb01e807847185a
Zheng Cao <chori84@gmail.com>
2018-05-30 10:22:55 -0700

    Implement Toggle Present

--- src/components/Student.js
+++ src/components/Student.js
@@ -3,6 +3,13 @@ import PropTypes from 'prop-types';
 import './Student.css';
 
 class Student extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isPresent: true
+    };
+  }
 
   static propTypes = {
     name: PropTypes.string.isRequired,
@@ -15,6 +22,14 @@ class Student extends Component {
       return 'top-student';
     }
     return 'student'
+  }
+
+  onToggleButton = () => {
+    // const isPresent = !this.state.isPresent;
+    this.setState({
+      // isPresent,
+      isPresent: !this.state.isPresent
+    });
   }
 
   render() {
@@ -31,8 +46,12 @@ class Student extends Component {
       <article
       className={this.isTopStudent()}>
         <h3>{studentName}</h3>
+        <h4>
+          {this.state.isPresent? "In class" : "Absent"}
+        </h4>
         <p>Age: {studentAge}</p>
         <p>Email: {studentEmail}</p>
+        <button onClick={ this.onToggleButton }>Toggle Present</button>
       </article>
     );
   }
